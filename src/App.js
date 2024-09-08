40ce942a3ff89060005788f22d6b3ab6236727c9
Zipory <ITZipory5@gmail.com>
2024-09-08 13:54:30 +0300

    first branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Welcom from './Components/Welcom';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <Welcom/>
       </header>
     </div>
   );
---
+++ src/Components/Welcom.jsx
@@ -0,0 +1,9 @@
+function Welcom() {
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    )
+}
+
+export default Welcom
