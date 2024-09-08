e31a5c3770e5457015ea72fd580a02749c10dfb1
BENI23LAHAV <beni0548472300@gmail.com>
2024-09-08 14:13:05 +0300

    yom zeva'a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-
+import "./App.css";
+import Yom_zvaa from "./Components/Yom_zvaa";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <header className="App-header"></header>
+      <Yom_zvaa />
     </div>
   );
 }
