68dc9fb922fc7fa3b0cee3ae126fb8c0a63c729a
Zipory <ITZipory5@gmail.com>
2024-09-08 14:23:35 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Welcom from './Components/Welcom';
+import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Welcom/>
+      <Home/>
       </header>
     </div>
   );
